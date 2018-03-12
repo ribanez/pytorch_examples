a093973a45e79dd2983de2663205bb49a3e3a7f3
ribanez <mail@ribanez.cl>
2018-03-11 23:25:24 -0300

    modify model_mist net to sequential

--- CNN_Mnist/Model_Mnist.py
+++ CNN_Mnist/Model_Mnist.py
@@ -21,19 +21,32 @@ class CNN(nn.Module):
     """
     def __init__(self):
         super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
+        self.conv1 = nn.Sequential(nn.Conv2d(in_channel =1,
+                                             out_channel = 10,
+                                             kernel_size=5),
+                                   nn.ReLU(),
+                                   nn.MaxPool2d(kernel_size = 2)
+                                   )
+        self.conv2 = nn.Sequential(nn.Conv2d(in_channel = 10,
+                                             out_channel = 20,
+                                             kernel_size=5),
+                                   nn.ReLU(),
+                                   nn.MaxPool2d(kernel_size = 2),
+                                   nn.Dropout2d(p = 0.3)
+                                   )
+
+        self.out = nn.Sequential(nn.Linear(in_features =320,
+                                           out_features = 50),
+                                 nn.Linear(in_features = 50,
+                                           out_features = 10)
+                                 )
 
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.drop(self.conv2(x)), 2))
+        x = self.conv1(x)
+        x = self.conv2(x)
         x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
+        x = self.out(x)
+
         return F.log_softmax(x,  dim=1)
 
     def __name__(self):
