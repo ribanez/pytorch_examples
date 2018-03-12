de647c1896a22f2bd5b9cffe840dcefff90791a7
ribanez <mail@ribanez.cl>
2018-03-11 22:17:05 -0300

    add requirement

--- CNN_Mnist/main.py
+++ CNN_Mnist/main.py
@@ -6,7 +6,7 @@ from torchvision import transforms
 import torchvision.datasets as dset
 from torch.utils.data.sampler import SubsetRandomSampler
 
-from CNN_Mnist.Model_Mnist import Model_Mnist
+from Model_Mnist import Model_Mnist
 
 
 ## RETRAIN
